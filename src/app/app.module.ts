df4b783c6300a2bbc33a693f4656b6f4f27cec76
Asen Nikolaev <asen.nikolaev@nevexis.com>
2021-04-22 16:23:15 +0300

    init homescreen component and delete app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeScreenComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeScreenComponent]
 })
 export class AppModule { }
