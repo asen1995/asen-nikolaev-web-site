c05539f16ad48bd2e1e5009d9048ccdb73afd8b9
Asen Nikolaev <asen.nikolaev@nevexis.com>
2021-04-26 13:10:11 +0300

    footer implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { HomeScreenComponent } from './home-screen/home-screen.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     HomeScreenComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
