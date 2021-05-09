a7aed93ea7acd7180740efb9a82eecb73d75363e
Asen Nikolaev <asen.nikolaev@nevexis.com>
2021-05-09 15:04:03 +0300

    adding BrowserAnimationsModule  to enable animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { HomeScreenComponent } from './home-screen/home-screen.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoadingSceenComponent } from './loading-sceen/loading-sceen.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
+    
     HomeScreenComponent,
     NavigationBarComponent,
     FooterComponent,
     LoadingSceenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [HomeScreenComponent]
